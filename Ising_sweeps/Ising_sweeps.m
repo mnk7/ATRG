4b56aa2381bad4a11bda680976ed770ece4d9f61
Michael Nunhofer <michael.nunhofer@ur.de>
2020-08-21 00:34:22 +0200

    Revert "Trying original ATRG splittings"
    This reverts commit 81346ccfa091fd2a2457894c57e1697c76aa7e26.

--- Ising_sweeps/Ising_sweeps.m
+++ Ising_sweeps/Ising_sweeps.m
@@ -2,7 +2,7 @@
 clear all;
 clc;
 
-inputMC = dlmread('Sweep_n32_t32_d1_b0.100000_J1.000000_B0.000000.dat', ' ', 0, 0);
+inputMC = dlmread('Sweep_n2_t2_d1_b0.100000_J1.000000_B0.000000.dat', ' ', 0, 0);
 inputHOTRG = dlmread('HOTRG_Ising_sweep_L2.dat', '\t', 0, 0);
 inputATRG = dlmread('Ising_sweep.dat', ' ', 0, 0);
 
